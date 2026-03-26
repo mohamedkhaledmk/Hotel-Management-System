36562e1369d4ebf1900473478f9ee4e037997495
mohamedkhaledmk <khaled25256@gmail.com>
2026-03-26 09:49:58 +0200

    Login Repository

---
+++ Repositories/Interfaces/ILoginRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelManagementSystem.Repositories.Interfaces
+{
+     public interface ILoginRepository
+    {
+        bool Verify(string tableName, string username, string password);
+    }
+}
---
+++ Repositories/LoginRepository.cs
@@ -0,0 +1,24 @@
+﻿using Dapper;
+using HotelManagementSystem.Data;
+using HotelManagementSystem.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelManagementSystem.Repositories
+{
+    public class LoginRepository : ILoginRepository
+    {
+        public bool Verify(string tableName, string username, string password)
+        {
+            string t = tableName.ToLower() == "kitchen" ? "kitchen" : "Frontends";
+            using var conn = DbConnectionFactory.GetLoginConnection();
+            int res = conn.ExecuteScalar<int>(
+                $"""
+                select count(1) from {t} where username=@username and password=@password
+                """
+                , new {username,password});
+            return res > 0;
+        }
+    }
+}
