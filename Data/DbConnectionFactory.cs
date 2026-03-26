257ff0cab5805c74adb74cec75a3f08227870be8
mohamedkhaledmk <khaled25256@gmail.com>
2026-03-26 09:50:20 +0200

    Db connedction factory dp

---
+++ Data/DbConnectionFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace HotelManagementSystem.Data
+{
+    public static class DbConnectionFactory
+    {
+        public static SqlConnection GetFrontendConnection() =>
+            new SqlConnection(
+                ConfigurationManager.ConnectionStrings["FrontendReservationConnection"]?.ConnectionString
+                );
+        public static SqlConnection GetLoginConnection() =>
+            new SqlConnection(
+                ConfigurationManager.ConnectionStrings["LoginManagerConnection"]?.ConnectionString
+            );
+    }
+}
